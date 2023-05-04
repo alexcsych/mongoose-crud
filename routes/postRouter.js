072b7f9fcecd5c2b48cad22729667083530300a7
Alexc <alexsejsi4@gmail.com>
2023-05-04 19:16:40 +0300

    Update router

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -3,6 +3,15 @@ const { postController } = require('../controller');
 
 const postRouter = Router();
 
-postRouter.get('/', postController.getPosts);
+postRouter
+  .route('/')
+  .get(postController.getPosts)
+  .post(postController.createPost);
+
+postRouter
+  .route('/:postId')
+  .get(postController.getPostById)
+  .patch(postController.updatePostById)
+  .delete(postController.deletePostById);
 
 module.exports = postRouter;
